09182500ccdbb08ae6258a61a3e1f792b48df49c
mattdykstra <matt.dykstra@yahoo.com>
2017-10-30 14:47:47 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -70,9 +70,17 @@ var app = {
 	SpinnerDialog.show('Please Wait','Loading latest products...',true);
 	app.popup = cordova.InAppBrowser.open('http://pospro.co','_blank','location=no,toolbar=no,zoom=no'); 
 	app.popup.addEventListener("exit", function() {
-	  //app.popup.show();
-	  //SpinnerDialog.hide();
-		  
+		navigator.notification.confirm(
+                'Do you want to exit app?',  
+                function(i){
+                    if(i==2)
+                     {
+                       navigator.app.exitApp(); //This will Close the App
+                     }
+                },              
+                'App Name',            
+                'Cancel,Exit'          
+              );
 	});  
 	SpinnerDialog.hide();
 	     
