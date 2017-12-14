8dc60ae0af0b86c8e2dfafae18358eb83e9c00c7
mattdykstra <matt.dykstra@yahoo.com>
2017-12-14 18:07:19 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -9,7 +9,7 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        app.popup = window.open('http://pospro.co','_self','location=no,toolbar=no,zoom=no'); 
+        app.popup = cordova.InAppBrowser.open('http://pospro.co','_self','location=no,toolbar=no,zoom=no'); 
 	app.popup.addEventListener("exit", function() {
              navigator.app.exitApp();
 	});
